cdc6285e8f4838053033518469fb14ccf61ade35
Max Tweddell <maxt.nz@gmail.com>
2017-06-10 21:41:46 +1200

    henlo world

--- example/src/clj_discord_example/core.clj
+++ example/src/clj_discord_example/core.clj
@@ -1,19 +1,33 @@
 (ns clj-discord-example.core
   (:gen-class)
-  (:require [clj-discord.core :as discord]))
+  (:require [clj-discord.core :as discord]
+            [clj-discord-example.forecast :refer :all]
+            [clj-http.client :as client]
+            [clojure.string :as str]
+            ))
 
 (defonce token (.trim (slurp "token.txt")))
 
 (defn d100 [type data]
   (discord/answer-command data "!d100" (str "Here you are a random number between 1 and 100: " (+ (rand-int 100) 1))))
+(defn d20 [type data]
+  (discord/answer-command data "!d20" (str "Here you are, a random number between 1 and 10 " (+ (rand-int 20) 1))))
+
+(defn command-test [type data]
+  (let [command (get data "content")]
+    (discord/answer-command data "!blop" (str "blop " (str command)))
+    ))
+(defn weather [type data]
+  (let [command (get data "content")]
+    (discord/answer-command data "!weather" (str  "hi " (:humidity (:currently (forecast "37" "22")))))))
 
 (defn log-event [type data] 
   (println "\nReceived: " type " -> " data))
 
 (defn -main [& args]
   (discord/connect token 
-                   {"MESSAGE_CREATE" [d100]
-                    "MESSAGE_UPDATE" [d100]
+                   {"MESSAGE_CREATE" [d20 d100 weather command-test]
+                    "MESSAGE_UPDATE" [d20 d100 weather command-test]
                     ; "ALL_OTHER" [log-event]
                     }
                    true))
--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "2.3.0"]
                  [org.clojure/data.json "0.2.6"]
+                 [forecast-clojure "1.0.3"]
                  [stylefruits/gniazdo "1.0.0"]]
   :main clj-discord.core
-  :profiles {:uberjar {:aot [clj-discord.core]}})
+  :profiles {:uberjar {:aot :all}}
+)
