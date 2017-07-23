bb4291c7b0c9cb7a6e076b5de18e849fbbd7e483
Max Tweddell <maxt.nz@gmail.com>
2017-07-23 17:01:44 +1200

    more cleanups

--- project.clj
+++ project.clj
@@ -1,17 +1,8 @@
 (defproject clj-discord-example "0.1.0-SNAPSHOT"
   :dependencies [[clj-discord "0.1.1-SNAPSHOT"]
                  [cheshire "5.7.1"]
-                 [org.clojure/java.jdbc "0.7.0-alpha3"]
-                 [java-jdbc/dsl "0.1.3"]
                  [clj-http "3.6.1"]
-                 [clojail "1.0.6"]
-                 [org.clojure/tools.nrepl "0.2.13"]
-                 [org.clojure/data.json "0.2.6"]
-                 [com.layerware/hugsql "0.4.7"]
-                 [org.postgresql/postgresql "9.4.1212"]
                  [environ "1.1.0"]]
-  :plugins [[lein-kibit "0.1.5"] [lein-cljfmt "0.5.6"]]
   :main clj-discord-example.core
-  :profiles {:uberjar
-             {:aot [clj-discord-example.core]}}
-  :user {:env {:forecast-key "e0971c18e4fe3fa0bf9cbb286be291b6"} })
+  :profiles {:uberjar {:aot [clj-discord-example.core]}}
+  :user {:env {:forecast-key "e0971c18e4fe3fa0bf9cbb286be291b6"}})
--- src/clj_discord_example/core.clj
+++ src/clj_discord_example/core.clj
@@ -1,63 +1,35 @@
 (ns clj-discord-example.core
   (:gen-class)
   (:require [clj-discord.core :as discord]
-            [clj-discord-example.repl :as repl]
             [clj-discord-example.forecast :refer :all]
             [clj-http.client :as client]
-            [cheshire.core :as json]
-
-            [clj-discord-example.db :refer [db]]
-            [clj-discord-example.db.messages :as messages]
-
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            ))
 
 (defonce token (.trim (slurp "token.txt")))
 
 (defn d100 [type data]
-  (discord/answer-command data "!d100" (str "Here you are a random number between 1 and 100: " (inc (rand-int 100)))))
+  (discord/answer-command data "!d100" (str "Here you are a random number between 1 and 100: " (+ (rand-int 100) 1))))
 (defn d20 [type data]
-  (discord/answer-command data "!d20" (str "here u are a randem numbur bigger than 1 (one)  but littler than tweny sex: " (inc (rand-int 27)))))
+  (discord/answer-command data "!d20" (str "Here you are, a random number between 1 and 10 " (+ (rand-int 20) 1))))
 
 (defn command-test [type data]
   (let [command (get data "content")]
-    (discord/answer-command data "!blop" (str "blop " (str command)))))
+    (discord/answer-command data "!blop" (str "blop " (str command)))
+    ))
 (defn weather [type data]
   (let [command (get data "content")]
-    (discord/answer-command data "!humidity" (str  "hi " (:humidity (:currently (forecast "37" "22")))))))
+    (discord/answer-command data "!weather" (str  "hi " (:humidity (:currently (forecast "37" "22")))))))
 
-(defn lmgtfy [data command]
-  (let [channel_id (get data "channel_id") message (get data "id")]
-    (discord/answer-command data "lmgtfy" (str "http://lmgtfy.com/?q=darude+sandstorm" (str/replace command " " "+")))))
-
-(defn void [type data]
-  (let [server (get data "channel_id")]
-    (if (= server "324776471883415552")
-      (discord/delete-message data))))
-
-(defn getRandomNumber [type data]
-  (let [command (get data "content")]
-    (discord/answer-command data "getRandomNumber()" (str "Here you are, a random number : " 4))))
-
-(defn rot13 [type data]
-  (let [command (get data "content")
-        args (str/join " " (rest (str/split (get data "content") #" ")))]
-    (discord/answer-command data "encrypt" (clojure.string/join (map char (map (fn [x] (- x 5)) (map int args)))))))
-(defn mum [type data]
-  (let [command (get data "content")]
-    (discord/answer-message data "mom " "mum")))
-(defn oaky [type data]
-  (let [commmand (get data "content")]
-    (discord/answer-message data "oaky"  "https://68.media.tumblr.com/15208297a50932cccbef51a5dbeb47bf/tumblr_inline_ojfxuvC1RV1qierqv_540.jpg")))
-(defn log-event [type data]
-  (do
-    (println "\nReceived: " type " -> " data)
-    (messages/insert-message db {:message (json/generate-string data)})))
-
-(defn repler [type data]
-  (let [command (get data "content") args (str/join " " (rest (str/split (get data "content") #" ")))]
-    (discord/answer-command data "eval" (try  (eval (read-string args)) (catch Exception e (println "uh oh"))))))
+(defn log-event [type data] 
+  (println "\nReceived: " type " -> " data))
 
 (defn -main [& args]
-  (discord/connect token {"MESSAGE_UPDATE" [d20 d100 weather command-test void log-event], "MESSAGE_CREATE" [d20 d100 weather command-test void getRandomNumber mum log-event oaky repler rot13]} true))
+  (discord/connect token 
+                   {"MESSAGE_CREATE" [d20 d100 weather command-test]
+                    "MESSAGE_UPDATE" [d20 d100 weather command-test]
+                    ; "ALL_OTHER" [log-event]
+                    }
+                   true))
 
 ;(discord/disconnect)
